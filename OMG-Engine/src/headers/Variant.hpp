bf679cc5771c1aafea3c0be54b87f11d9a95f2f5
Daniel Fernández Marqués <daniFM333@gmail.com>
2017-12-06 19:58:44 +0100

    Updated header comments

--- OMG-Engine/src/headers/Variant.hpp
+++ OMG-Engine/src/headers/Variant.hpp
@@ -1,4 +1,12 @@
- 
+/*
+Author: Daniel Fernandez Marques
+Date: 28/11/2017
+
+Copyright (c) 2017 OMG-Engine
+
+Subject to the license described in LICENSE file
+*/
+
 #pragma once
 
 #include <string>
